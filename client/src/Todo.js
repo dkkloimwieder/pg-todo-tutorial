5c9e83188fdb5630f732eaae2f9303d25f599074
d kloimwieder <dkkloimwieder@gmail.com>
2021-10-10 21:12:22 -0400

    revert source to beginning of section-3a

--- client/src/Todo.js
+++ client/src/Todo.js
@@ -1,61 +1,9 @@
 import React from 'react';
-import { UPDATE_COMPLETED, DELETE_TODO, GET_TODOS } from './graphql';
-import { useMutation } from '@apollo/client';
 
 export default function Todo({ id, task, completed }) {
-  const [updateCompleted] = useMutation(UPDATE_COMPLETED);
 
-  const [deleteTodo] = useMutation(DELETE_TODO, {
-    update: (cache, mutationResult) => {
-      cache.evict({
-        id: `Todo:${mutationResult.data.deleteTodoById.deletedTodoId}`,
-      });
-      cache.gc();
-    },
-  });
-  /*const { todos } = cache.readQuery({ query: GET_TODOS });
-      cache.writeQuery({
-        query: GET_TODOS,
-        data: {
-          todos: todos.filter(
-            (todo) =>
-              todo.id !== mutationResult.data.deleteTodoById.deletedTodoId
-          ),
-        },
-      })
-
-      cache.modify({
-        id: 'ROOT_QUERY',
-        fields: {
-          todos(existingRefs) {
-            return existingRefs.filter(
-              (todo) =>
-                todo.__ref !==
-                `Todo:${mutationResult.data.deleteTodoById.deletedTodoId}`
-            );
-          },
-        },
-      });
-      cache.gc();
-    },
-  });*/
-
-  const handleDeleteClick = async (id) => {
-    try {
-      await deleteTodo({ variables: { id: id } });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  const handleCompleteClick = async (id) => {
-    try {
-      await updateCompleted({
-        variables: { id: id, completed: !completed },
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const handleDeleteClick =  () => {};
+  const handleCompleteClick =  () => {};
 
   return (
     <li className="todo-list__element">
@@ -68,13 +16,13 @@ export default function Todo({ id, task, completed }) {
       <div className="todo-list__button-container">
         <button
           className="todo-list__button"
-          onClick={() => handleCompleteClick(id)}
+          onClick={() => handleCompleteClick()}
         >
           Complete
         </button>
         <button
           className="todo-list__button"
-          onClick={() => handleDeleteClick(id)}
+          onClick={() => handleDeleteClick()}
         >
           Delete
         </button>
--- client/src/TodoList.js
+++ client/src/TodoList.js
@@ -1,27 +1,10 @@
 import React, { useState } from 'react';
 import Todo from './Todo';
-import { useQuery, useMutation } from '@apollo/client';
-import { GET_TODOS, CREATE_TODO } from './graphql';
 
 export default function TodoList() {
   const [input, setInput] = useState('');
-  const [createTodo] = useMutation(CREATE_TODO, {
-    update: (cache, mutationResult) => {
-      const todosQuery = cache.readQuery({ query: GET_TODOS });
-      cache.writeQuery({
-        query: GET_TODOS,
-        data: {
-          todos: todosQuery.todos.concat(mutationResult.data.createTodo.todo),
-        },
-      });
-    },
-  });
-  const { data, error, loading } = useQuery(GET_TODOS);
-  if (error) return <p className="alert">{error.message}</p>;
-  if (loading) return <p className="alert">loading...</p>;
-  const { todos } = data;
 
-  //const todos = [{ task: 'replace meeeee!', id: '1', completed: false }];
+  const todos = [{ task: 'replace meeeee!', id: '1', completed: false }];
   return (
     <div>
       <ul className="todo-list">
@@ -31,17 +14,7 @@ export default function TodoList() {
       </ul>
       <form
         className="todo-list__form"
-        onSubmit={async (e) => {
-          e.preventDefault();
-          try {
-            await createTodo({
-              variables: { task: input },
-            });
-          } catch (error) {
-            console.error(error);
-          }
-          setInput('');
-        }}
+        onSubmit={ (e) => {}}
       >
         <input
           className="todo-list__input"
